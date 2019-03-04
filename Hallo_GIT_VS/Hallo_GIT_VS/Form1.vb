6c370e6d03a5256f773aa0fb2e3ba4daf11fb9c2
Victor Santana <victor.santana@latschbacher.ch>
2019-03-04 16:24:42 +0100

    Alles ok?

--- Hallo_GIT_VS/Hallo_GIT_VS/Form1.vb
+++ Hallo_GIT_VS/Hallo_GIT_VS/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("Hallo Welt!")
+        MsgBox("Hallo Welt! Alles Ok?")
     End Sub
 End Class
